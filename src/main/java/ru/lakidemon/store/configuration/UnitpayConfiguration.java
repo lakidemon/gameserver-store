7a291f54476c03de7cfb137bb06153af1451d213
lakidemon <forregistrationsmail@mail.ru>
2020-09-17 21:14:41 +0700

    Add public key to configuration

--- src/main/java/ru/lakidemon/store/configuration/UnitpayConfiguration.java
+++ src/main/java/ru/lakidemon/store/configuration/UnitpayConfiguration.java
@@ -2,9 +2,6 @@ package ru.lakidemon.store.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.lakidemon.store.repository.PaymentsRepository;
-import ru.lakidemon.store.unitpay.UnitpayService;
-import ru.lakidemon.store.unitpay.UnitpayServiceImpl;
 
 import java.util.List;
 
@@ -12,12 +9,18 @@ import java.util.List;
 public class UnitpayConfiguration {
 
     @Bean
-    String secretCode() {
+    String secretKey() {
+        return "";
+    }
+
+    @Bean
+    String publicKey() {
         return "";
     }
 
     @Bean
     List<String> allowedIPs() {
-        return List.of("31.186.100.49", "178.132.203.105", "52.29.152.23", "52.19.56.234");
+        return List.of("31.186.100.49", "178.132.203.105", "52.29.152.23", "52.19.56.234", "127.0.0.1");
     }
+
 }
