5a5ddd8ea66648baa0e3e481fd898d6f1e0ea7ed
lakidemon <forregistrationsmail@mail.ru>
2020-09-23 23:41:45 +0700

    Remove OrderService.canOrder method

--- src/main/java/ru/lakidemon/store/service/OrderService.java
+++ src/main/java/ru/lakidemon/store/service/OrderService.java
@@ -7,8 +7,6 @@ public interface OrderService {
 
     Order createOrder(String customer, Item item);
 
-    boolean canOrder(String customer, Item what);
-
     boolean dispatchOrder(Order order);
 
 }
