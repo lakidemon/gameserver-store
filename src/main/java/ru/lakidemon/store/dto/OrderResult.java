a07ca7538c9ef14ce069e7452e2e51aff55f0acc
lakidemon <forregistrationsmail@mail.ru>
2020-10-02 00:09:27 +0700

    Fix OrderResult serialization

--- src/main/java/ru/lakidemon/store/dto/OrderResult.java
+++ src/main/java/ru/lakidemon/store/dto/OrderResult.java
@@ -1,8 +1,12 @@
 package ru.lakidemon.store.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.Value;
 
 @Builder
+@Value
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderResult {
     private String paymentLink;
     private String message;
